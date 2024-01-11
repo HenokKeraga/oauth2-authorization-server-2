47e56bd9253958769e113e1001cd986f8423bf3e
henok keraga <henokkeraga@gmail.com>
2024-01-11 08:45:37 -0700

    oauth2 authorization server

---
+++ src/main/java/com/example/authorizationserver4/AuthorizationServer4Application.java
@@ -0,0 +1,13 @@
+package com.example.authorizationserver4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationServer4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationServer4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/authorizationserver4/AuthorizationServer4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authorizationserver4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServer4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
